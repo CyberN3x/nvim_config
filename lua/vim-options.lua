8901a6d1a871a5a5c0cea96b72567b9bcdcdb39b
nex <nexaura@proton.me>
2024-01-27 20:45:34 -0500

    modified:   lazy-lock.json 	modified:   lua/vim-options.lua

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,5 +3,7 @@ vim.cmd("set tabstop=3")
 vim.cmd("set softtabstop=3")
 vim.cmd("set shiftwidth=3")
 vim.cmd("set number")
+vim.cmd("set textwidth=100")
+vim.cmd("set spell")
 vim.g.mapleader = " "
 
